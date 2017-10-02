5d370036ba96faf6ce06a1bb072f7dfe6c1b831c
Chris Regan <chris@regan.co.nz>
2017-10-02 22:00:00 +1300

    still trying to get operationdetails children other than gvid!!

--- src/serviceOpMerge/package-info.java
+++ src/serviceOpMerge/package-info.java
@@ -11,7 +11,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
-import org.jdom2.Attribute;
+//import org.jdom2.Attribute;
 import org.jdom2.Document;
 import org.jdom2.Element;
 import org.jdom2.JDOMException;
@@ -32,20 +32,26 @@ class recommendationcollator {
 		
 		
 		Element rootElement = document.getRootElement();
+		System.out.println("Root element: " + rootElement.getName());
+		
 		Element childElement1 = rootElement.getChild(null);
+		System.out.println("childElement1: " + childElement1);
+		
 		Element childElement2 = childElement1.getChild(null);
-		List listedChildren1 = childElement1.getChildren(null);
+		System.out.println("childElement2: " + childElement2);
+		
+		Element childElement3 = childElement2.getChild(null);
+		System.out.println("childElement3: " + childElement3);
+		
+		
+		List<Element> listedChildren1 = childElement1.getChildren(null);
+		System.out.println("listedChildren1: " + listedChildren1);
+		System.out.println("listedChildren1: " + listedChildren1.toArray());
+		//System.out.println("Doc Content: " + document.getDescendants());
+				
 		//String cappedPrice = childElement2.getAttribute("cappedPrice").getValue();
 		//Element cappedPriceElement = childElement2.getChild(cappedPrice);
-		System.out.println("Root element: " + document.getRootElement().getName());
-		System.out.println("Root element: " + rootElement.getName());
-		System.out.println("CPR Testing Output: " + childElement1);
-		System.out.println("CPR Testing Output: " + childElement2);
-		System.out.println("CPR Testing Output: " + listedChildren1);
 		//System.out.println("CPR Testing Output: " + childElement2.);
-		
-		
-		
 		//List<Element> Operations = classElement.getChildren();
 		//System.out.println("Childern of Root :" + classElement.getChildren().toString());
 		//System.out.println("Childern of Root :" + Operations.getChildren());		
