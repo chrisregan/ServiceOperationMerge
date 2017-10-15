eacb1f9b1e7e035e758b648827c87e926fd06bde
Chris Regan <chris@regan.co.nz>
2017-10-15 20:26:44 +1300

    more progress on getting values of children and adding them together as integers. addedd values with decimal format is broken though

--- src/serviceOpMerge/package-info.java
+++ src/serviceOpMerge/package-info.java
@@ -10,6 +10,7 @@ package serviceOpMerge;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
+import java.lang.*;
 
 import org.jdom2.Content;
 import org.jdom2.Attribute;
@@ -17,7 +18,10 @@ import org.jdom2.Document;
 import org.jdom2.Element;
 import org.jdom2.JDOMException;
 import org.jdom2.filter.ElementFilter;
+import org.jdom2.filter.Filter;
 import org.jdom2.input.SAXBuilder;
+
+import java.text.DecimalFormat;
 
 class recommendationcollator {
 	
@@ -40,11 +44,11 @@ class recommendationcollator {
 		System.out.println("childElement1: " + childElement1);
 		
 		//Element Filter variables - super useful for like everything!
-		ElementFilter operationdetails = new org.jdom2.filter.ElementFilter("operationdetails");
+		//ElementFilter operationdetails = new org.jdom2.filter.ElementFilter("operationdetails");
 		ElementFilter gvid = new org.jdom2.filter.ElementFilter("gvid");
 		ElementFilter servicerecommendation = new org.jdom2.filter.ElementFilter("servicerecommendation");
 		ElementFilter invoiceline = new org.jdom2.filter.ElementFilter("invoiceline");
-		ElementFilter cappedprice = new org.jdom2.filter.ElementFilter("cappedprice");
+		//ElementFilter cappedprice = new org.jdom2.filter.ElementFilter("cappedprice");
 		ElementFilter desc = new org.jdom2.filter.ElementFilter("desc");
 		
 		
@@ -56,23 +60,65 @@ class recommendationcollator {
 		Element childElement3 = childElement2.getChild(null);
 		System.out.println("childElement3: " + childElement3);
 		
+		Element parentOperationdetails = rootElement.getChild("operationdetails"); // gets the first 'operationdetails'
+		Element opcode = parentOperationdetails.getChild("opcode");
+		String opcodeValue = opcode.getValue();
+		Element cappedprice = parentOperationdetails.getChild("cappedprice"); // gets the first 'cappedprice'
+		String cappedPriceString = cappedprice.getValue();
+		float cappedpriceValue = Float.valueOf(cappedPriceString);
+		System.out.println("Capped Price for " + opcodeValue + " is: " + cappedpriceValue);
+		
+		
+		//Create an Element Variable which is all "ServiceRecommendation" Childs
+		Element servicerecommendationElement = parentOperationdetails.getChild("servicerecommendations");
+		
+		//Make a list of the children Elements of servicerecommendationElement
+		List <Element> servicerecommendationList = servicerecommendationElement.getChildren();
+		System.out.println("Service Recommendation List: " + servicerecommendationList);
+
+		//Count the Number of SR's in the List 
+		int servicerecommendationElementCount = servicerecommendationList.size();
+		System.out.println("No of Child Service Recommendations: " + servicerecommendationElementCount);
+		
+		//Get the first SR at array 0
+		Element servicerecommendationListFirst = servicerecommendationList.get(0);
+		
+		//Get the value of the SR at 0
+		String servicerecommendationListFirstValue = servicerecommendationListFirst.getValue();
+		
+		//Get Children of SR 0
+		List <Element> SR0 = servicerecommendationListFirst.getChildren();
+			System.out.println("SR0" + SR0);
+		//Get capped Price for SR 0
+		Element SR0Price = servicerecommendationListFirst.getChild("cappedprice");
+		String SR0cappedPrice = SR0Price.getValue();
+		float SR0cappedPriceValue = Float.valueOf(SR0cappedPrice);
+			System.out.println("SR0 Capped Price is: " + SR0cappedPriceValue);
+			
+		float totalprice = (cappedpriceValue+SR0cappedPriceValue);
+		DecimalFormat format = new DecimalFormat();
+		format.setMaximumIntegerDigits(2);
+			java.lang.System.out.println("Total Price is: " + (format.format(totalprice)));
+				
+		System.out.println("1st Service Recommendation : " + servicerecommendationListFirstValue);
+			
 		
 		List<Element> listedChildren1 = childElement1.getChildren(null);
 		System.out.println("listedChildren1: " + listedChildren1);
 		//System.out.println("listedChildren1: " + listedChildren1.toArray());
 		
 		//Parent Service Capped Price
-		for(Element operationdetailsElement : rootElement.getDescendants(operationdetails)) {
-		    System.out.println("Parent Capped Price: " +operationdetailsElement.getContent(cappedprice));
-		    Content cappedpricecontent =  operationdetailsElement.getContent(18);
-		    System.out.println("Parent Capped Price: " +cappedpricecontent);
+		//for(Element operationdetailsElement : rootElement.getDescendants(operationdetails)) {
+		//    System.out.println("Parent Capped Price: " +operationdetailsElement.getContent(cappedprice));
+		//    Content cappedpricecontent =  operationdetailsElement.getContent(18);
+		//    System.out.println("Parent Capped Price: " +cappedpricecontent);
 		    
-		}
+		//}
 		
 		//Service Recommendations Capped Prices
-		for(Element servicerecommendationElement : rootElement.getDescendants(servicerecommendation)) {
-		    System.out.println("servicerecommendations Content?: " +servicerecommendationElement.getContent(cappedprice));
-		}
+		//for(Element servicerecommendationElement : rootElement.getDescendants(servicerecommendation)) {
+		//    System.out.println("servicerecommendations Content?: " +servicerecommendationElement.getContent(cappedprice));
+		//}
 		//Invoice Lines
 		//for(Element invoicelineElement : rootElement.getDescendants(invoiceline)) {
 		//    System.out.println("invoicelines?: " +invoicelineElement.getContent(desc));
